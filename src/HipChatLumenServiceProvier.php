c32036547a9df785f0d25b532d6b50a2bba5b614
Augusto Gerardo Sotelo Labarca <augusto.sotelo@dreamcode.cl>
2017-05-02 10:39:02 -0300

    Added service provider for Lumen 5. (#1)
    Updated installation notes.'

---
+++ src/HipChatLumenServiceProvier.php
@@ -0,0 +1,31 @@
+<?php
+namespace Bestit\HipChat;
+
+use Illuminate\Support\ServiceProvider;
+use Bestit;
+
+/**
+ * 
+ * @author Augusto Sotelo L. <agsotelo@gmail.com>
+ *
+ */
+class HipChatLumenServiceProvider extends ServiceProvider {
+	
+	/**
+	 * Register the service provider and alias
+	 */
+	public function register() {
+		$this->app->singleton('hipchat', function ($app) {
+			return new Client(config('hipchat.api_token'), config('hipchat.server_url'));
+		});
+		
+		$this->app->alias(Bestit\HipChat\Facade\HipChat::class, 'HipChat');
+	}
+	
+	/**
+	 * Load the configuration
+	 */
+	public function boot() {
+		$this->app->configure('hipchat');
+	}
+}
